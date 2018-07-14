07f7b3afe83d8f458a193a194450b2a33ab01b75
zzzmfps <zzz100826@gmail.com>
2018-07-14 20:00:50 +0800

    Update 1029 Median (25).cpp

--- Advanced-Level-Practice/1029 Median (25).cpp
+++ Advanced-Level-Practice/1029 Median (25).cpp
@@ -8,27 +8,36 @@ static int x = []() {
     return 0;
 }();
 
-int main(void)  // AC but only gives me 16 scores, why???
+class Solution  // AC but only gives me 16/25 scores, why???
 {
+  private:
     int n1, n2;
     vector<int> seq;
 
-    cin >> n1;
-    seq.resize(n1);
-    for (int i = 0; i < n1; ++i) cin >> seq[i];
+  public:
+    Solution()
+    {
+        cin >> n1;
+        seq.resize(n1);
+        for (int i = 0; i < n1; ++i) cin >> seq[i];
 
-    cin >> n2;
-    seq.resize((n1 + n2 + 1) / 2);
-    for (int i = seq.size() - 1; i >= n1; --i) cin >> seq[i];
+        cin >> n2;
+        seq.resize((n1 + n2 + 1) / 2);
+        for (int i = seq.size() - 1; i >= n1; --i) cin >> seq[i];
 
-    int mark = n1 - 1, tmp;
-    for (int i = n1 + n2 - seq.size(); i > 0; --i, --mark) {
-        cin >> tmp;
-        if (tmp >= seq[mark]) break;
-        seq[mark] = tmp;
-        if (mark == 0) break;
+        int mark = n1 - 1, tmp;
+        for (int i = n1 + n2 - seq.size(); i > 0; --i, --mark) {
+            cin >> tmp;
+            if (tmp >= seq[mark]) break;
+            seq[mark] = tmp;
+            if (mark == 0) break;
+        }
+        cout << max(max(seq[mark], seq[mark + 1]), seq.back());
     }
-    cout << max(max(seq[mark], seq[mark + 1]), seq.back());
+};
 
+int main(void)
+{
+    Solution s;
     return 0;
 }
