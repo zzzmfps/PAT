0c954878ec9c7c62eaafff0cccb506d5d896d6d9
zzzmfps <zzz100826@gmail.com>
2018-07-26 21:36:13 +0800

    Update 1051 Pop Sequence (25).cpp

--- Advanced-Level-Practice/1051 Pop Sequence (25).cpp
+++ Advanced-Level-Practice/1051 Pop Sequence (25).cpp
@@ -1,3 +1,4 @@
+// APPROACH 1. check the sequence directly. //
 #include <iostream>
 #include <set>
 using namespace std;
@@ -8,7 +9,7 @@ static int x = []() {
     return 0;
 }();
 
-class Solution  // check sequence directly. approach 2: simply simulating the progress.
+class Solution
 {
   private:
     int capacity, length, seqs;
@@ -52,3 +53,35 @@ int main(void)
     s.checkSequences();
     return 0;
 }
+
+// APPROACH 2. simply simulates the progress. //
+#include <iostream>
+#include <stack>
+class Solution
+{
+  private:
+    int capacity, length, seqs;
+
+  public:
+    Solution() { (cin >> capacity >> length >> seqs).get(); }
+
+    void checkSequences()
+    {
+        stack<int> st;
+        while (seqs--) {
+            int tmp, i = 0;
+            while (cin.peek() != '\n') {
+                cin >> tmp;
+                if (!st.empty() && st.top() > tmp) break;
+                if (st.empty() || st.top() < tmp) {
+                    if (tmp - i + st.size() > capacity) break;
+                    while (i < tmp) st.push(++i);
+                }
+                if (st.top() == tmp) st.pop();
+            }
+            cout << (i == length && st.empty() ? "YES" : "NO") << '\n';
+            while (!st.empty()) st.pop();
+            cin.ignore(0x7FFFFFFF, '\n');
+        }
+    }
+};
