d2aa4792449fe21b8c65263502aa244d5ddbd8db
zzzmfps <zzz100826@gmail.com>
2020-12-31 15:48:43 +0800

    new: add L2-033

---
+++ GroupRank-Practice/L2/L2-033 简单计算器 (25分).cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+template <typename T> stack<T> getStack(int size)
+{
+    stack<T> st;
+    for (int i = 0; i < size; ++i) {
+        st.emplace(0);
+        cin >> st.top();
+    }
+    return st;
+}
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    stack<int> nums = getStack<int>(n);
+    stack<char> ops = getStack<char>(n - 1);
+    for (int i = n - 1; i > 0; --i) {
+        int y = nums.top();
+        char op = ops.top();
+        nums.pop(), ops.pop();
+        switch (op) {
+            case '+':
+                nums.top() += y;
+                break;
+            case '-':
+                nums.top() -= y;
+                break;
+            case '*':
+                nums.top() *= y;
+                break;
+            case '/':
+                if (0 == y) {
+                    cout << "ERROR: " << nums.top() << "/0";
+                    return 0;
+                }
+                nums.top() /= y;
+        }
+    }
+    cout << nums.top();
+    return 0;
+}
