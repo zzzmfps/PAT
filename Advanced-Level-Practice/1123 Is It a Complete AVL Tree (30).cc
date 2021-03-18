7e150280d03404b1f7e95d6e3dc989a04580970e
zzzmfps <zzz100826@gmail.com>
2021-03-18 18:33:57 +0800

    update: 1123

--- Advanced-Level-Practice/1123 Is It a Complete AVL Tree (30).cc
+++ Advanced-Level-Practice/1123 Is It a Complete AVL Tree (30).cc
@@ -1,85 +1,14 @@
-#include <iostream>
-#include <queue>
-using namespace std;
+#include <bits/stdc++.h>
 
-class Solution
-{
-private:
-    struct Node
-    {
+class Avl {
+public:
+    struct Node {
         int data, height;
         Node *left, *right;
         Node(int d, int h = 1) : data(d), height(h), left(nullptr), right(nullptr) {}
     };
-    Node *avl = nullptr;
 
-public:
-    Solution()
-    {
-        int n;
-        cin >> n;
-        for (int i = 0, v; i < n; ++i) {
-            cin >> v;
-            insert(avl, v);
-        }
-    }
-
-    void isCompleteAvlTree()
-    {
-        queue<Node *> q;
-        q.push(avl);
-        bool isComplete = true, flag = false;
-        while (!q.empty()) {
-            for (int i = q.size(); i > 0; --i) {
-                auto x = q.front();
-                cout << (x == avl ? "" : " ") << x->data;
-                if (x->left) {
-                    if (flag) isComplete = false;
-                    q.push(x->left);
-                } else {
-                    flag = true;
-                }
-                if (x->right) {
-                    if (flag) isComplete = false;
-                    q.push(x->right);
-                } else {
-                    flag = true;
-                }
-                q.pop();
-            }
-        }
-        cout << '\n' << (isComplete ? "YES" : "NO");
-    }
-
-private:
-    int getHeight(Node *root) { return (root ? root->height : 0); }
-
-    int getBalanceFactor(Node *root) { return getHeight(root->left) - getHeight(root->right); }
-
-    void updateHeight(Node *root) { root->height = 1 + max(getHeight(root->left), getHeight(root->right)); }
-
-    void rightRotate(Node *&root)
-    {
-        Node *tmp = root->left;
-        root->left = tmp->right;
-        tmp->right = root;
-        updateHeight(root);
-        updateHeight(tmp);
-        root = tmp;
-    }
-
-    void leftRotate(Node *&root)
-    {
-        Node *tmp = root->right;
-        root->right = tmp->left;
-        tmp->left = root;
-        updateHeight(root);
-        updateHeight(tmp);
-        root = tmp;
-    }
-
-    void insert(Node *&root, int val)
-    {
+    static void insert(Node *&root, int val) {
         if (!root) {
             root = new Node(val);
         } else if (val < root->data) {
@@ -98,10 +27,81 @@ private:
             }
         }
     }
+
+private:
+    static int getHeight(Node *root) {
+        return (root ? root->height : 0);
+    }
+
+    static int getBalanceFactor(Node *root) {
+        return getHeight(root->left) - getHeight(root->right);
+    }
+
+    static void updateHeight(Node *root) {
+        root->height = 1 + std::max(getHeight(root->left), getHeight(root->right));
+    }
+
+    static void rightRotate(Node *&root) {
+        Node *tmp = root->left;
+        root->left = tmp->right;
+        tmp->right = root;
+        updateHeight(root);
+        updateHeight(tmp);
+        root = tmp;
+    }
+
+    static void leftRotate(Node *&root) {
+        Node *tmp = root->right;
+        root->right = tmp->left;
+        tmp->left = root;
+        updateHeight(root);
+        updateHeight(tmp);
+        root = tmp;
+    }
 };
 
-int main(void)
-{
+class Solution {
+private:
+    Avl::Node *avl = nullptr;
+
+public:
+    Solution() {
+        int n;
+        std::cin >> n;
+        for (int i = 0, v; i < n; ++i) {
+            std::cin >> v;
+            Avl::insert(avl, v);
+        }
+    }
+
+    void isCompleteAvlTree() {
+        std::queue<Avl::Node *> q;
+        q.push(avl);
+        bool isComplete = true, flag = false;
+        while (!q.empty()) {
+            for (int i = q.size(); i > 0; --i) {
+                auto x = q.front();
+                std::cout << (x == avl ? "" : " ") << x->data;
+                if (x->left) {
+                    if (flag) isComplete = false;
+                    q.push(x->left);
+                } else {
+                    flag = true;
+                }
+                if (x->right) {
+                    if (flag) isComplete = false;
+                    q.push(x->right);
+                } else {
+                    flag = true;
+                }
+                q.pop();
+            }
+        }
+        std::cout << '\n' << (isComplete ? "YES" : "NO");
+    }
+};
+
+int main(void) {
     Solution s;
     s.isCompleteAvlTree();
     return 0;
