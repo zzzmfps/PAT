8fe0ffe25cfec6b8712b0e1e1616e96a6389dd00
BHHZ-PC <zzz100826@gmail.com>
2021-04-08 18:26:37 +0800

    add: L3-020

---
+++ GPLT-Practice/L3/L3-020 至多删三个字符 (30).cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using ULL = unsigned long long;
+
+class RemoveChars {
+public:
+    static ULL subStringNums(const std::string &str) {
+        ULL ne01 = (str[0] != str[1]);
+        ULL ne02 = (str[0] != str[2]);
+        ULL ne12 = (str[1] != str[2]);
+
+        std::vector<ULL *> dp(5);
+        dp[0] = new ULL[4]{1, 0, 0, 0};
+        dp[1] = new ULL[4]{1, 1, 0, 0};
+        dp[2] = new ULL[4]{1, 1 + ne01, 1, 0};
+        dp[3] = new ULL[4]{1, 1 + ne01 + ne12, 1 + ne01 + (ne02 & ne12), 1};
+        dp[4] = new ULL[4]{1};
+
+        for (size_t i = 3; i < str.size(); ++i) {
+            for (size_t j = 1; j <= 3; ++j) {
+                dp[4][j] = dp[3][j - 1] + dp[3][j];
+                for (size_t k = 1; k <= j; ++k) {
+                    if (str[i] == str[i - k]) {
+                        dp[4][j] -= dp[3 - k][j - k];
+                        break;
+                    }
+                }
+            }
+            for (size_t i = 1; i < dp.size(); ++i) std::swap(dp[i - 1], dp[i]);
+        }
+        return RemoveChars::done(dp);
+    }
+
+private:
+    static ULL done(std::vector<ULL *> &v) {
+        size_t resIndex = v.size() - 2;
+        ULL res = 0; // std::accumulate DOESN'T supports unsigned long long
+        for (size_t i = 0; i < 4; ++i) res += v[resIndex][i];
+        for (auto &&ptr : v) delete[] ptr;
+        return res;
+    }
+};
+
+int main() {
+    std::string str;
+    std::getline(std::cin, str);
+    std::cout << RemoveChars::subStringNums(str);
+    return 0;
+}
